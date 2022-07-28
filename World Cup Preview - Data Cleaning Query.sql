d7b26c5db8846ec4f20672a0bef12b10d6777f48
LowCodeDataGirl <105794651+LowCodeDataGirl@users.noreply.github.com>
2022-07-28 07:33:05 -0700

    Add files via upload

---
+++ World Cup Preview - Data Cleaning Query.sql
@@ -0,0 +1,42 @@
+ --Query to Clean Data in this Table 
+
+/* 
+Query to Update country Names
+This Query Updates the table with the correct country names
+*/
+
+UPDATE  [Portfolio_Projects ].[dbo].[WorldCups_preview]
+SET
+      [Winner]  = 'Germany'
+	 
+where [Winner] = 'Germany FR'
+
+
+UPDATE  [Portfolio_Projects ].[dbo].[WorldCups_preview]
+SET
+      [Runners-Up]  = 'Germany'
+	 
+where [Runners-Up] = 'Germany FR'
+
+
+UPDATE  [Portfolio_Projects ].[dbo].[WorldCups_preview]
+SET
+      [Third]  = 'Germany'
+	 
+where [Third] = 'Germany FR'
+
+
+UPDATE  [Portfolio_Projects ].[dbo].[WorldCups_preview]
+SET
+      [Fourth]  = 'Germany'
+	 
+where [Fourth] = 'Germany FR'
+
+
+UPDATE  [Portfolio_Projects ].[dbo].[WorldCups_preview]
+SET
+      [Fourth]  = 'South Korea'
+	 
+where [Fourth] = 'Korea Republic'
+
+
