4b2d581d2d5fa950bb806333e32d1e842f035f34
LowCodeDataGirl <105794651+LowCodeDataGirl@users.noreply.github.com>
2022-08-03 06:16:04 -0700

    Update Insights Query -World Cup Dataset.sql

--- Insights Query -World Cup Dataset.sql
+++ Insights Query -World Cup Dataset.sql
@@ -89,3 +89,73 @@ Order by [SumHalfTimeGoals] DESC
 
 --Do any teams seem to be stronger in either the first half or the second half? (think about both offense and defense)
 
+Do any teams seem to be stronger in either the first half or the second half? (think about both offense and defense)
+ I had to create a CTE to create the table with the tables i needed to answer this question.
+The name of the CTE is HalfTimeGoals and I got the 2nd half goals by subtracting Home team goals and 1st half goals 
+ */
+
+WITH HalfTimeGoals   --This query gives the result of the home teams with the most first half wins    
+
+([Home Team Name],
+[1st Half Home Goals],
+[2nd Half Home Goals],
+[Home Team Goals],
+[Away Team Name],
+[1st Half Away Goals],
+[2nd Half Away Goals],
+[Away Team Goals]
+)
+AS
+(
+SELECT     [Home Team Name],
+           cast([Half-time Home Goals]as int) as [1st Half Home Goals],
+           cast([Home Team Goals] as int)-cast([Half-time Home Goals] as int) as [2nd Half Home Goals],
+		   cast([Home Team Goals]as int),
+		   [Away Team Name],
+		   cast([Half-time Away Goals]as int) as [1st Half Away Goals],
+		   cast([Away Team Goals]as int)- cast([Half-time Away Goals]as int) as [2nd Half Away Goals],
+		   cast([Away Team Goals]as int)
+
+ FROM [Portfolio_Projects].[dbo].[WorldCupMatches]
+ )
+
+ SELECT [Home Team Name], COUNT([1st Half Home Goals]) AS [Number of 1st Half Home Wins]
+ FROM  HalfTimeGoals 
+ WHERE [1st Half Home Goals] > [1st Half Away Goals]--the format for offense was when 
+ GROUP BY [Home Team Name]
+ ORDER BY [Number of 1st Half Home Wins] DESC
+ 
+
+WITH HalfTimeGoals   --This query gives the result of the away teams with the most first half wins    
+
+([Home Team Name],
+[1st Half Home Goals],
+[2nd Half Home Goals],
+[Home Team Goals],
+[Away Team Name],
+[1st Half Away Goals],
+[2nd Half Away Goals],
+[Away Team Goals]
+)
+AS
+(
+SELECT     [Home Team Name],
+           cast([Half-time Home Goals]as int) as [1st Half Home Goals],
+           cast([Home Team Goals] as int)-cast([Half-time Home Goals] as int) as [2nd Half Home Goals],
+		   cast([Home Team Goals]as int),
+		   [Away Team Name],
+		   cast([Half-time Away Goals]as int) as [1st Half Away Goals],
+		   cast([Away Team Goals]as int)- cast([Half-time Away Goals]as int) as [2nd Half Away Goals],
+		   cast([Away Team Goals]as int)
+
+ FROM [Portfolio_Projects].[dbo].[WorldCupMatches]
+ )
+
+ SELECT [Home Team Name], COUNT([1st Half Away Goals]) AS [Number of 1st Half Away Wins]
+ FROM  HalfTimeGoals 
+ WHERE [1st Half Away Goals] > [2nd Half Away Goals]--the format for offense was when 
+ GROUP BY [Home Team Name]
+ ORDER BY [Number of 1st Half Away Wins] DESC
+ 
+
+--NOTE: Find a query to sum the results of both query by Country 
